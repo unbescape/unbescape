d533779edf1161c3db559f598f683472c9e19704
Daniel Fernández <daniel.fernandez@11thlabs.org>
2014-04-07 21:41:47 +0200

    Added test for unknown NCR

---
+++ src/test/java/org/unbescape/html/HtmlEscape09Test.java
@@ -0,0 +1,57 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2014, The UNBESCAPE team (http://www.unbescape.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.unbescape.html;
+
+import org.junit.Test;
+
+import static org.unbescape.html.HtmlEscapeTestUtil.testUnescape;
+
+/**
+ * 
+ * @author Daniel Fern&aacute;ndez
+ * 
+ * @since 1.0
+ *
+ */
+public class HtmlEscape09Test {
+
+
+    private static final String TEXT = "a&NotFound;z";
+
+
+
+
+    @Test
+    public void testUnescape01() throws Exception {
+
+        testUnescape(TEXT, TEXT);
+
+    }
+
+
+
+
+    public HtmlEscape09Test() {
+        super();
+    }
+
+
+}
+
