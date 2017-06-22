a3b6779abdce06e4c4096e35e574bbfa6607a4e6
Daniel Fernández <daniel.fernandez@11thlabs.org>
2017-06-22 23:12:56 +0200

    Added class org.unbescape.Unbescape in order to report the version of the library being used

---
+++ src/main/java/org/unbescape/ClassLoaderUtils.java
@@ -0,0 +1,251 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2014, The UNBESCAPE team (http://www.unbescape.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.unbescape;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * <p>
+ *   Utility class for obtaining a correct classloader on which to operate from a
+ *   specific class.
+ * </p>
+ * 
+ * @author Daniel Fern&aacute;ndez
+ * 
+ * @since 1.1.6
+ *
+ */
+final class ClassLoaderUtils {
+
+
+    private static final ClassLoader classClassLoader;
+    private static final ClassLoader systemClassLoader;
+    private static final boolean systemClassLoaderAccessibleFromClassClassLoader;
+
+
+    static {
+        classClassLoader = getClassClassLoader(ClassLoaderUtils.class);
+        systemClassLoader = getSystemClassLoader();
+        systemClassLoaderAccessibleFromClassClassLoader = isKnownClassLoaderAccessibleFrom(systemClassLoader, classClassLoader);
+    }
+
+
+
+
+    /**
+     * <p>
+     *   Obtain a resource by name, throwing an exception if it is not present.
+     * </p>
+     * <p>
+     *   First the <em>context class loader</em> will be used. If this class loader is not
+     *   able to locate the resource, then the <em>class class loader</em>
+     *   (<tt>ClassLoaderUtils.class.getClassLoader()</tt>) will be used if it is different from
+     *   the thread context one. Last, the System class loader will be tried.
+     * </p>
+     * <p>
+     *   This method does never return <tt>null</tt>.
+     * </p>
+     *
+     * @param resourceName the name of the resource to be obtained.
+     * @return an input stream on the resource (null never returned).
+     * @throws IOException if the resource could not be located.
+     *
+     */
+    static InputStream loadResourceAsStream(final String resourceName) throws IOException {
+
+        final InputStream inputStream = findResourceAsStream(resourceName);
+        if (inputStream != null) {
+            return inputStream;
+        }
+
+        // No way to obtain that resource, so we must raise an IOException
+        throw new IOException("Could not locate resource '" + resourceName + "' in the application's class path");
+
+    }
+
+
+    /**
+     * <p>
+     *   Try to obtain a resource by name, returning <tt>null</tt> if it could not be located.
+     * </p>
+     * <p>
+     *   This method works very similarly to {@link #loadResourceAsStream(String)} but will just return <tt>null</tt>
+     *   if the resource cannot be located by the sequence of class loaders being tried.
+     * </p>
+     *
+     * @param resourceName the name of the resource to be obtained.
+     * @return an input stream on the resource, or <tt>null</tt> if it could not be located.
+     *
+     */
+    static InputStream findResourceAsStream(final String resourceName) {
+
+        // First try the context class loader
+        final ClassLoader contextClassLoader = getThreadContextClassLoader();
+        if (contextClassLoader != null) {
+            final InputStream inputStream = contextClassLoader.getResourceAsStream(resourceName);
+            if (inputStream != null) {
+                return inputStream;
+            }
+            // Pass-through, there might be other ways of obtaining it
+            // note anyway that this is not really normal: the context class loader should be
+            // either able to resolve any of our application's resources, or to delegate to a class
+            // loader that can do that.
+        }
+
+        // The thread context class loader might have already delegated to both the class
+        // and system class loaders, in which case it makes little sense to query them too.
+        if (!isKnownLeafClassLoader(contextClassLoader)) {
+
+            // The context class loader didn't help, so... maybe the class one?
+            if (classClassLoader != null && classClassLoader != contextClassLoader) {
+                final InputStream inputStream = classClassLoader.getResourceAsStream(resourceName);
+                if (inputStream != null) {
+                    return inputStream;
+                }
+                // Pass-through, maybe the system class loader can do it? - though it would be *really* weird...
+            }
+
+            if (!systemClassLoaderAccessibleFromClassClassLoader) {
+
+                // The only class loader we can rely on for not being null is the system one
+                if (systemClassLoader != null && systemClassLoader != contextClassLoader && systemClassLoader != classClassLoader) {
+                    final InputStream inputStream = systemClassLoader.getResourceAsStream(resourceName);
+                    if (inputStream != null) {
+                        return inputStream;
+                    }
+                    // Pass-through, anyway we have a return null after this...
+                }
+
+            }
+
+        }
+
+        return null;
+
+    }
+
+
+
+
+    /*
+     * This will return the thread context class loader if it is possible to access it
+     * (depending on security restrictions)
+     */
+    private static ClassLoader getThreadContextClassLoader() {
+        try {
+            return Thread.currentThread().getContextClassLoader();
+        } catch (final SecurityException se) {
+            // The SecurityManager prevents us from accessing, so just ignore it
+            return null;
+        }
+    }
+
+
+    /*
+     * This will return the class class loader if it is possible to access it
+     * (depending on security restrictions)
+     */
+    private static ClassLoader getClassClassLoader(final Class<?> clazz) {
+        try {
+            return clazz.getClassLoader();
+        } catch (final SecurityException se) {
+            // The SecurityManager prevents us from accessing, so just ignore it
+            return null;
+        }
+    }
+
+
+    /*
+     * This will return the system class loader if it is possible to access it
+     * (depending on security restrictions)
+     */
+    private static ClassLoader getSystemClassLoader() {
+        try {
+            return ClassLoader.getSystemClassLoader();
+        } catch (final SecurityException se) {
+            // The SecurityManager prevents us from accessing, so just ignore it
+            return null;
+        }
+    }
+
+
+    /*
+     * This method determines whether it is known that a this class loader is a a child of another one, or equal to it.
+     * The "known" part is because SecurityManager could be preventing us from knowing such information.
+     */
+    private static boolean isKnownClassLoaderAccessibleFrom(final ClassLoader accessibleCL, final ClassLoader fromCL) {
+
+        if (fromCL == null) {
+            return false;
+        }
+
+        ClassLoader parent = fromCL;
+
+        try {
+
+            while (parent != null && parent != accessibleCL) {
+                parent = parent.getParent();
+            }
+
+            return (parent != null && parent == accessibleCL);
+
+        } catch (final SecurityException se) {
+            // The SecurityManager prevents us from accessing, so just ignore it
+            return false;
+        }
+
+    }
+
+
+    /*
+     * This method determines whether it is known that a this class loader is a "leaf", in the sense that
+     * going up through its hierarchy we are able to find both the class class loader and the system class
+     * loader. This is used for determining whether we should be confident on the thread-context class loader
+     * delegation mechanism or rather try to perform class/resource resolution manually on the other class loaders.
+     */
+    private static boolean isKnownLeafClassLoader(final ClassLoader classLoader) {
+
+        if (classLoader == null) {
+            return false;
+        }
+
+        if (!isKnownClassLoaderAccessibleFrom(classClassLoader, classLoader)) {
+            // We cannot access the class class loader from the specified class loader, so this is not a leaf
+            return false;
+        }
+
+        // Now we know there is a way to reach the class class loader from the argument class loader, so we should
+        // base or results on whether there is a way to reach the system class loader from the class class loader.
+        return systemClassLoaderAccessibleFromClassClassLoader;
+
+    }
+
+
+
+    
+    private ClassLoaderUtils() {
+        super();
+    }
+    
+    
+    
+}
---
+++ src/main/java/org/unbescape/Unbescape.java
@@ -0,0 +1,115 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2014, The UNBESCAPE team (http://www.unbescape.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.unbescape;
+
+import java.util.Properties;
+
+
+/**
+ * <p>
+ *   Class meant to keep some constants related to the version of the Unbescape library being used, build date, etc.
+ * </p>
+ *
+ * @author Daniel Fern&aacute;ndez
+ *
+ * @since 1.1.6
+ *
+ */
+public final class Unbescape {
+
+    public static final String VERSION;
+    public static final String BUILD_TIMESTAMP;
+
+    public static final int VERSION_MAJOR;
+    public static final int VERSION_MINOR;
+    public static final int VERSION_BUILD;
+    public static final String VERSION_TYPE;
+
+
+    static {
+
+        String version = null;
+        String buildTimestamp = null;
+        try {
+            final Properties properties = new Properties();
+            properties.load(ClassLoaderUtils.loadResourceAsStream("org/unbescape/unbescape.properties"));
+            version = properties.getProperty("version");
+            buildTimestamp = properties.getProperty("build.date");
+        } catch (final Exception ignored) {
+            // Ignored: we don't have such information, might be due to IDE configuration
+        }
+
+        VERSION = version;
+        BUILD_TIMESTAMP = buildTimestamp;
+
+        if (VERSION == null || VERSION.trim().length() == 0) {
+
+            VERSION_MAJOR = 0;
+            VERSION_MINOR = 0;
+            VERSION_BUILD = 0;
+            VERSION_TYPE = "UNKNOWN";
+
+        } else {
+
+            try {
+
+                String versionRemainder = VERSION;
+
+                int separatorIdx = versionRemainder.indexOf('.');
+                VERSION_MAJOR = Integer.parseInt(versionRemainder.substring(0,separatorIdx));
+                versionRemainder = versionRemainder.substring(separatorIdx + 1);
+
+                separatorIdx = versionRemainder.indexOf('.');
+                VERSION_MINOR = Integer.parseInt(versionRemainder.substring(0, separatorIdx));
+                versionRemainder = versionRemainder.substring(separatorIdx + 1);
+
+                separatorIdx = versionRemainder.indexOf('.');
+                if (separatorIdx < 0) {
+                    separatorIdx = versionRemainder.indexOf('-');
+                }
+                VERSION_BUILD = Integer.parseInt(versionRemainder.substring(0, separatorIdx));
+                VERSION_TYPE = versionRemainder.substring(separatorIdx + 1);
+
+            } catch (final Exception e) {
+                throw new ExceptionInInitializerError(
+                        "Exception during initialization of Unbescape versioning utilities. Identified Unbescape " +
+                        "version is '" + VERSION + "', which does not follow the {major}.{minor}.{build}[.|-]{type} " +
+                        "scheme");
+            }
+
+        }
+
+    }
+
+
+    public static boolean isVersionStableRelease() {
+        return "RELEASE".equals(VERSION_TYPE);
+    }
+
+
+
+
+
+
+    private Unbescape() {
+        super();
+    }
+
+}
